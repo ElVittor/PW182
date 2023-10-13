1c3bc525463f9baec87ecd1246000ef9814deee1
ElVittor <93359780+ElVittor@users.noreply.github.com>
2023-10-13 16:42:32 -0600

    se creo la base de la practica
    se crearon las rutas y la clase

--- expoblade/resources/views/welcome.blade.php
+++ expoblade/resources/views/welcome.blade.php
@@ -1,15 +1,15 @@
-@component('componentes.layout')
+{{--@component('components.layout')
     <x-slot name="title">
        Holis Title
     </x-slot>
     <h1>HOLA COMPAÑEROS</h1>
-@endcomponent
+@endcomponent--}}
     
-{{--<X-layout>
-   <x-slot name="title">
-       Holis Title
+<X-layout>
+    <x-slot name="title">
+    Holis Title
     </x-slot>
     <h1>HOLA COMPAÑEROS</h1>
     <p>Lorem ipsum dolor sit a</p>
-</X-layout>--}}
+</X-layout>
 {{--notar que en esta forma ya se asume donde esta el componente, y solo llamamos su nombre "layout"--}}
